1f446d2e3b41f536628465b41499d8c5a3d8aed3
tomasaalima <tomasabdias22@gmail.com>
2024-10-09 23:25:57 -0300

    feature: inicia projeto labsolos-server-dotnet8

---
+++ backend/LabSolos-Server-DotNet8/Program.cs
@@ -0,0 +1,33 @@
+using LabSolos_Server_DotNet8.Database;
+using LabSolos_Server_DotNet8.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configurar o Swagger para documentação da API
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configurar o Entity Framework Core com SQLite
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Adicionar suporte para controladores
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configurar o pipeline de requisição
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// Usar mapeamento top-level para os controladores
+app.MapControllers();
+
+app.Run();
