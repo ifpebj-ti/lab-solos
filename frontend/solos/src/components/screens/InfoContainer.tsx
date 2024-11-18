8fb56f7113237ed39f49e807146a95c8a214a9f6
Ricardo Espindola <ricardoespindola128@gmail.com>
2024-11-18 15:54:39 -0300

    Construção tela de verificação de usuários cadastrados

--- frontend/solos/src/components/screens/InfoContainer.tsx
+++ frontend/solos/src/components/screens/InfoContainer.tsx
@@ -1,7 +1,7 @@
 type InfoItem = {
   title: string;
   value: string;
-  width: string; // Exemplo: '1/5', '2/5', etc.
+  width: string;
 };
 
 type InfoContainerProps = {
@@ -14,7 +14,8 @@ function InfoContainer({ items }: InfoContainerProps) {
       {items.map((item, index) => (
         <div
           key={index}
-          className={`h-full flex justify-evenly flex-col w-${item.width}`}
+          className={`h-full flex justify-evenly flex-col`}
+          style={{ width: item.width }}
         >
           <p className='font-inter-regular text-clt-1 text-sm'>{item.title}</p>
           <p className='font-inter-medium text-clt-2 text-ellipsis text-nowrap overflow-hidden whitespace-nowrap'>
