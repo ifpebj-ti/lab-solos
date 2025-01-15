b7103b17bce31c046e81d580a15e7002132436e0
Ricardo Espindola <ricardoespindola128@gmail.com>
2025-01-14 22:00:24 -0300

    finalização telas de mentorado

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,8 +1,14 @@
 import { useUser } from '@/components/context/UserProvider';
 import LoadingIcon from '../../public/icons/LoadingIcon';
+import OpenSearch from '@/components/global/OpenSearch';
+import { useState } from 'react';
+import FollowUpCard from '@/components/screens/FollowUp';
+import LayersIcon from '../../public/icons/LayersIcon';
+import InfoContainer from '@/components/screens/InfoContainer';
 
 function Profile() {
   const { rankID } = useUser();
+  const [loading, setLoading] = useState(false);
 
   if (rankID === null) {
     return (
@@ -14,18 +20,78 @@ function Profile() {
       </div>
     );
   }
+  const infoItems = [
+    {
+      title: 'Nome',
+      value: 'Maria Leony Silva',
+      width: '50%',
+    },
+    {
+      title: 'Email',
+      value: 'mls@ufcg.discente.edu.br',
+      width: '30%',
+    },
+    { title: 'Instituição', value: 'UFCG', width: '20%' },
+  ];
+  const infoItems2 = [{ title: 'Cidade', value: 'Patos - PB', width: '100%' }];
+  const infoItems3 = [
+    {
+      title: 'Telefone',
+      value: '(85) 98126-8744',
+      width: '100%',
+    },
+  ];
+  const infoItems4 = [
+    {
+      title: 'Status',
+      value: 'Liberado',
+      width: '100%',
+    },
+  ];
+  const infoItems5 = [
+    { title: 'Curso', value: 'Med. Veterinária', width: '100%' },
+  ];
 
   return (
-    <div className='flex justify-center w-full h-screen items-center flex-col gap-y-5'>
-      <h1>Profile</h1>
-      <div>Teste pipeline</div>
-      <nav>
-        {String(rankID) === '1' && <p>Menu para Admin</p>}
-        {String(rankID) === '2' && <p>Menu para Mentor</p>}
-        {String(rankID) === '3' && <p>Menu para Mentorado</p>}
-        {!String(rankID) && <p>Você precisa fazer login.</p>}
-      </nav>
-    </div>
+    <>
+      {loading ? (
+        <div className='flex justify-center flex-row w-full h-screen items-center gap-x-4 font-inter-medium text-clt-2 bg-backgroundMy'>
+          <div className='animate-spin'>
+            <LoadingIcon />
+          </div>
+          Carregando...
+        </div>
+      ) : (
+        <div className='w-full flex min-h-screen justify-start items-center flex-col overflow-y-auto bg-backgroundMy'>
+          <div className='w-11/12 flex items-center justify-between mt-7'>
+            <h1 className='uppercase font-rajdhani-medium text-3xl text-clt-2'>
+              Perfil
+            </h1>
+            <div className='flex items-center justify-between gap-x-6'>
+              <OpenSearch />
+            </div>
+          </div>
+          <div className='w-11/12 mt-7'>
+            <div className='flex gap-x-5 h-32'>
+              <FollowUpCard title='Empréstimos Realizados' number='22' icon={<LayersIcon />} />
+              <FollowUpCard title='Itens Utilizados' number='59' icon={<LayersIcon />} />
+            </div>
+            <div className='w-full mt-7'>
+              <InfoContainer items={infoItems} />
+              <div className='w-full flex gap-x-8 mt-5'>
+                <InfoContainer items={infoItems2} />
+                <InfoContainer items={infoItems3} />
+                <InfoContainer items={infoItems4} />
+                <InfoContainer items={infoItems5} />
+              </div>
+              <div className='w-full mt-5'>
+                <InfoContainer items={infoItems} />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
--- frontend/src/pages/history/MentoringHistory.tsx
+++ frontend/src/pages/history/MentoringHistory.tsx
@@ -10,6 +10,7 @@ import InfoContainer from '@/components/screens/InfoContainer';
 import ItemTable from '@/components/global/table/Item';
 import { getUserById } from '@/integration/Users';
 import { formatDate } from '@/function/date';
+import { useUser } from '../../components/context/UserProvider';
 
 interface IUsuario {
   instituicao: string;
@@ -35,6 +36,7 @@ function MentoringHistory() {
   const [user, setUser] = useState<IUsuario>();
   const itemsPerPage = 7;
   const id = 3;
+  const { rankID } = useUser();
 
   useEffect(() => {
     const fetchGetUserById = async () => {
@@ -109,7 +111,9 @@ function MentoringHistory() {
         <div className='w-full flex min-h-screen justify-start items-center flex-col overflow-y-auto bg-backgroundMy'>
           <div className='w-11/12 flex items-center justify-between mt-7'>
             <h1 className='uppercase font-rajdhani-medium text-3xl text-clt-2'>
-              Histórico de Mentorados
+              Histórico
+              {String(rankID) === '1' ||
+                (String(rankID) === '2' && <span>de Mentorados</span>)}
             </h1>
             <div className='flex items-center justify-between gap-x-6'>
               <OpenSearch />
