46351ca7cd2f5a1ea930776e7b1b262f45d4c9f8
tomasaalima <tomasabdias22@gmail.com>
2025-04-24 19:09:10 -0300

    Remove debug da variavel VITE_API_URL

--- frontend/src/services/BaseApi.tsx
+++ frontend/src/services/BaseApi.tsx
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
-// Verifica o valor atual de VITE_API_URL
-console.log('VITE_API_URL:', window.env?.VITE_API_URL);
-
 export const api = axios.create({
   baseURL: window.env?.VITE_API_URL || 'http://localhost:5000', // Valor padrão
 });
