61cbab7f66fb7d98cb0830253a552b963b4aa8a7
eduardo_duarte_kenopy <edxb@discente.ifpe.edu.br>
2025-11-08 10:52:39 -0300

    feture: Padronizando telas Home

--- frontend/src/components/global/Carousel.tsx
+++ frontend/src/components/global/Carousel.tsx
@@ -30,20 +30,19 @@ function Carousel({ informacoes, imageSrc }: ICarousel) {
         {Array.from({ length: 4 }, (_, i) => {
           const itemIndex = getCircularIndex(currentIndex + i - 1);
           // const scaleClass = i === 1 ? 'scale-110' : 'scale-100';
-
           return (
             <div
               key={itemIndex}
               className={`w-[40%] h-[45%] lg:w-[20%] lg:h-[40%] landscape:h-[40%] landscape:lg:h-[80%] landscape:md:flex items-center justify-center transition-transform shadow-sm`}
             >
-              <div className=' w-full h-full rounded-md flex flex-col xl:flex-row items-center justify-center border border-borderMy hover:bg-cl-table-item transition-all ease-in-out duration-200 gap-x-2 text-clt-2'>
+              <div className=' w-full h-full rounded-md flex flex-col xl:flex-row items-center justify-center hover:bg-green-100 transition-all ease-in-out duration-200 shadow-md gap-2'>
                 <img
                   alt='Imagem descritiva da informação'
                   src={imageSrc[itemIndex]}
                   className='w-3/12 h-auto landscape:w-2/12 landscape:md:w-4/12 landscape:lg:w-3/12 md:w-3/12 lg:w-3/12 m-1'
                 ></img>
                 <div className='w-full h-[40%] lg:w-7/12 flex items-center justify-center text-center py-2'>
-                  <p className='font-rajdhani-medium text-xs md:text-base xl:text-lg text-clt-1 text-wrap landscape:md:text-md p-2'>
+                  <p className='font-inter-medium uppercase text-clt-2 text-xs line-clamp-2 landscape:px-0'>
                     {informacoes[itemIndex]}
                   </p>
                 </div>
