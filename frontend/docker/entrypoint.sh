61ef23f9bc649702954714a634103980b6fccbfa
cLausx0 <david.almeida2707@gmail.com>
2025-12-08 02:52:45 -0300

    fix: corrigindo falha com a variavel VITE_API_URL

--- frontend/docker/entrypoint.sh
+++ frontend/docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Gera env.js a partir do template substituindo as variáveis
-envsubst < /usr/share/nginx/html/.env.template.js > /usr/share/nginx/html/.env.js
+envsubst < /usr/share/nginx/html/.env.template.js > /usr/share/nginx/html/env.js
 
 exec "$@"
